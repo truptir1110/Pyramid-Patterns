dc0862237f09b492c7ddb1b4dc6c1a451aa70ae7
truptir1110 <135428809+truptir1110@users.noreply.github.com>
2024-09-22 12:21:04 +0530

    Add files via upload

---
+++ program5.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+void main(){
+print("enter the number of rows");
+int rows=int.parse(stdin.readLineSync()!);
+
+for(int i=1;i<=rows;i++){
+  int num=1;
+  //space
+  for(int s=1;s<=rows-i;s++){
+    stdout.write("  ");
+  }
+  //pattern symbol
+  for(int j=1;j<=(2*i-1);j++){
+    stdout.write("$num ");
+    if(j>=i){
+      num--;
+    }else{
+      num++;
+    }
+
+  }
+  
+  print("");
+   }
+}
---
+++ program9.dart
@@ -0,0 +1,21 @@
+import "dart:io";
+void main(){
+
+print("enter the number of rows");
+int rows=int.parse(stdin.readLineSync()!);
+int num=1;
+for(int i=1;i<=rows;i++){
+  
+  //space
+  for(int s=1;s<=i-1;s++){
+    stdout.write("  ");
+  }
+  //pattern symbol
+  for(int j=1;j<=(2*(rows-i)+1);j++){
+    stdout.write("$num ");
+    num++;
+  }
+  
+  print("");
+   }
+}
